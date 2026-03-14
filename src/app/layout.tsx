f76862f47c492e25f91512142448b0031ac13614
David Darx <daviddarx@gmail.com>
2026-03-14 14:35:14 +0100

    fix layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           }}
         />
         {/* PWA: standalone mode on iOS */}
-        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black" />
         <meta name="apple-mobile-web-app-title" content="SUPI" />
         <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
